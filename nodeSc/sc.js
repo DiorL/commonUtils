09aad4d1319568a3c44bf0b993f6108e115c5d1c
lvdebing <2440785788@qq.com>
2020-09-16 18:15:59 +0800

    bug fix

--- nodeSc/sc.js
+++ nodeSc/sc.js
@@ -5,7 +5,7 @@ const os = require('os');
 
 let readFir = `# commonUtils
 commonUtils for project
-！！！注意：每次添加完函数需执行upReadme`
+！！！注意：每次添加完函数需执行upReadme  `
 
 const fRead = fs.createReadStream(path.join(__dirname,'../js/utils.js'))
 const fWrite = fs.createWriteStream(path.join(__dirname,'../README.md'))
@@ -24,10 +24,9 @@ let index = 1
 fWrite.write(readFir + os.EOL)
 objReadline.on('line',(line)=>{
   let str = trim(line.toString())
-  console.log(str)
   if(str.startsWith('//rdme')){
     let temp = 'line' + index.toString() + ':' + trim(str).split('//rdme')[1];
-    fWrite.write(temp + os.EOL)
+    fWrite.write(temp + '  ' + os.EOL)
   }
   index++;
 })
